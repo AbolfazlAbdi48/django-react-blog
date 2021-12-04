5242d9140c3dfbe8c789eca394ef61d8fdff797c
abolfazlabdi48 <dev.abolfazlabdi@gmail.com>
2021-12-04 22:16:07 +0330

    add media files url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
