e80724b99d51303007b54b039d0eedf7135b26ee
abolfazlabdi48 <dev.abolfazlabdi@gmail.com>
2021-12-13 23:17:58 +0330

    feat: add like and dislike articles

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet, ArticleViewSet
+from .views import (
+    CategoryViewSet,
+    ArticleViewSet,
+    LikeView
+)
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet)
@@ -8,6 +12,7 @@ router.register('articles', ArticleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('likes/', LikeView.as_view(), name='likes'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
